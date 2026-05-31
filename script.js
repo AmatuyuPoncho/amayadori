3ec456ad820ec18c8c1306ed5971ad8fa33256c3
AmatuyuPoncho <orabou.2525@gmail.com>
2026-06-01 00:19:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,154 +1,120 @@
 const normalFortunes = [
-{
-name:'ごろごろ日和',
-image:'images/gorogoro.PNG'
-},
-{
-name:'お茶日和',
-image:'images/ocha.PNG'
-},
-{
-name:'雨音日和',
-image:'images/amaoto.PNG'
-},
-{
-name:'読書日和',
-image:'images/dokusyo.PNG'
-},
-{
-name:'お散歩日和',
-image:'images/osanpo.PNG'
-},
-{
-name:'おやすみ日和',
-image:'images/oyasumi.PNG'
-},
-{
-name:'ひなたぼっこ日和',
-image:'images/hinatabokko.PNG'
-},
-{
-name:'おやつ日和',
-image:'images/oyatsu.PNG'
-},
-{
-name:'おたより日和',
-image:'images/otayori.PNG'
-},
-{
-name:'おえかき日和',
-image:'images/oekaki.PNG'
-},
-{
-name:'深呼吸日和',
-image:'images/shinkokyu.PNG'
-},
-{
-name:'おかたづけ日和',
-image:'images/okataduke.PNG'
-},
-{
-name:'ROM専日和',
-image:'images/romusen.PNG'
-}
+  {
+    name:'ごろごろ日和',
+    image:'images/gorogoro.PNG'
+  },
+  {
+    name:'お茶日和',
+    image:'images/ocha.PNG'
+  },
+  {
+    name:'雨音日和',
+    image:'images/amaoto.PNG'
+  },
+  {
+    name:'読書日和',
+    image:'images/dokusyo.PNG'
+  },
+  {
+    name:'お散歩日和',
+    image:'images/osanpo.PNG'
+  },
+  {
+    name:'おやすみ日和',
+    image:'images/oyasumi.PNG'
+  },
+  {
+    name:'ひなたぼっこ日和',
+    image:'images/hinatabokko.PNG'
+  },
+  {
+    name:'おやつ日和',
+    image:'images/oyatsu.PNG'
+  },
+  {
+    name:'おたより日和',
+    image:'images/otayori.PNG'
+  },
+  {
+    name:'おえかき日和',
+    image:'images/oekaki.PNG'
+  },
+  {
+    name:'深呼吸日和',
+    image:'images/shinkokyu.PNG'
+  },
+  {
+    name:'おかたづけ日和',
+    image:'images/okataduke.PNG'
+  },
+  {
+    name:'ROM専日和',
+    image:'images/romusen.PNG'
+  }
 ];
 
 const rareFortunes = [
-{
-name:'🌈虹の日和',
-image:'images/rare_niji.PNG'
-}
+  {
+    name:'🌈虹の日和',
+    image:'images/rare_niji.PNG'
+  }
 ];
-
 const superRareFortunes = [
-{
-name:'💎紫陽花の祝福',
-image:'images/sr_ajisai.PNG'
-}
+  {
+    name:'💎紫陽花の祝福',
+    image:'images/sr_ajisai.PNG'
+  }
 ];
 
 const legendaryFortunes = [
-{
-name:'⭐ぽたもの特等席',
-image:'images/legend_potamo.PNG'
-}
+  {
+    name:'⭐ぽたもの特等席',
+    image:'images/legend_potamo.PNG'
+  }
 ];
 
-
 function drawFortune(){
-
-const n = document.getElementById('name').value.trim();
-
-if(!n){
-alert('お名前を入力してください☁️');
-return;
-}
-
-const roll = Math.random();
-
-let f;
-let rarityClass;
-
-if(roll < 0.05){
-f = legendaryFortunes[0];
-rarityClass = "legendary";
-
-}else if(roll < 0.15){
-f = superRareFortunes[0];
-rarityClass = "superRare";
-
-}else if(roll < 0.40){
-f = rareFortunes[0];
-rarityClass = "rare";
-
-}else{
-f = normalFortunes[Math.floor(Math.random() * normalFortunes.length)];
-rarityClass = "normal";
-}
-
-document.getElementById('result').innerHTML = `
-
-<div id="captureArea" class="resultCard ${rarityClass}">
-
+  const n = document.getElementById('name').value.trim();
+  if(!n){
+    alert('お名前を入力してください☁️');
+    return;
+  }
+  const roll = Math.random();
+  let f;
+  if(roll < 0.05){
+    f = legendaryFortunes[0];
+  }else if(roll < 0.15){
+    f = superRareFortunes[0];
+  }else if(roll < 0.40){
+    f = rareFortunes[0];
+  }else{
+    f = normalFortunes[
+      Math.floor(Math.random() * normalFortunes.length)
+    ];
+  }
+  document.getElementById('result').innerHTML = `
+<div class="resultCard">
 <div class="username">
 ☁️ ${n}さん ☁️
 </div>
-
 <div class="fortuneBadge">
 ${f.name}
 </div>
-
-<div class="text">
-${f.text}
+<button class="saveButton"
+onclick="showImage('${f.image}')">
+🖼️ 画像を見る </button>
 </div>
-
-<div class="comment">
-ぽたもより<br>
-${f.comment}
-</div>
-
-<button class="saveButton" onclick="saveResult()">
-💾 結果を保存
-</button>
-
-</div>
-`;
+  `;
 }
-
+function showImage(imageFile){
+  window.open(imageFile,'_blank');
+}
 function saveResult(){
-
-const element = document.getElementById('captureArea');
-
-html2canvas(element).then(canvas => {
-
-const link = document.createElement('a');
-
-link.download = 'poncho-result.png';
-
-link.href = canvas.toDataURL('image/png');
-
-link.click();
-
-});
-
+  const element = document.getElementById('captureArea');
+  html2canvas(element).then(canvas => {
+    const link = document.createElement('a');
+    link.download = 'poncho-result.png';
+    link.href = canvas.toDataURL('image/png');
+    link.click();
+  });
 }
